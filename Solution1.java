d112f5ca75da8b1cb729201bd3d2f628d35d2f2f
nishanthkulal <nishathkulal65462@gmail.com>
2022-08-11 19:55:36 -0700

    commit1

---
+++ Solution1.java
@@ -0,0 +1,16 @@
+import java.util.*;  
+class Solution1  
+{  
+public static void main(String[] args)  
+{  
+    String str;
+Scanner sc = new Scanner(System.in);  
+int s = sc.nextInt();
+double d = sc.nextDouble();
+ sc.nextLine();
+ str = sc.nextLine();  
+System.out.println("String: "+str); 
+System.out.println("Double: "+d); 
+System.out.println("Int: "+s);             
+}  
+}  
